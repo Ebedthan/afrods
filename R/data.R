a9fac9e6d6b0f57e1de3adab6204bc26fa4bc550
Anicet Ebou <anicet.ebou@gmail.com>
2018-08-24 20:30:01 +0000

    Adding docs

---
+++ R/data.R
@@ -0,0 +1,104 @@
+#' @importFrom tibble tibble
+NULL
+
+#' Agriculture and environment
+#'
+#'
+#' Agriculture and environment data from gapminder.
+#'
+#'
+#' @format A data frame with 27 variables: \code{country}, \code{region},
+#'   \code{year}, \code{}
+#'
+#'
+"agriculture_environment"
+
+#' Economy
+#'
+#' Economics data of African countries from Gapminder.
+#'
+#'
+#'
+#'
+#'
+"economy"
+
+#' Education
+#'
+#'
+#' Education data of African countries from Gapminder
+#'
+#'
+#'
+#'
+"education"
+
+#' Energy
+#'
+#'
+#' Energy data of African countries from Gapminder
+#'
+#'
+#'
+"energy"
+
+#' Health
+#'
+#'
+#'
+#' Health data of African countries from Gapminder
+#'
+#'
+#'
+#'
+"health"
+
+#' Infrastructure
+#'
+#'
+#'
+#' Infrastructure data of African countries from Gapminder.
+#'
+#'
+#'
+"infrastructure"
+
+#' Population
+#'
+#'
+#'
+#' Population data of African countires from Gapminder.
+#'
+#'
+"population"
+
+#' Society
+#'
+#'
+#'
+#' Society data of African countries from Gapminder.
+#'
+#'
+#'
+"society"
+
+#' Technology
+#'
+#'
+#'
+#' Technology data of African countries from Gapminder.
+#'
+#'
+#'
+"technology"
+
+#' Work
+#'
+#'
+#'
+#' Work data of African countries from Gapminder.
+#'
+#'
+#'
+"work"
+
