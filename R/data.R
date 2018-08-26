4cf6d65d129f2adcba41df7fdbaf4e9267da790c
Anicet Ebou <anicet.ebou@gmail.com>
2018-08-26 16:13:55 +0000

    Updating man

--- R/data.R
+++ R/data.R
@@ -6,10 +6,36 @@ NULL
 #'
 #' Agriculture and environment data from gapminder.
 #'
+#' @source https://gapminder.org/data
+#' @format Data frame with columns
+#' \describe{
+#' \item{Agriculture, value added}{% of GDP}
+#' \item{Agricultural land}{% of land area}
+#' \item{CO2 emissions}{kg per 2005 PPP $ of GDP}
+#' \item{Forest area}{sq. km}
+#' \item{Proportion of the population using improved drinking water sources, total}{}
+#' \item{Sulfur Emission per person}{kg/year}
+#' \item{Total sulfur emission}{kilotonnes/year}
+#' \item{Agricultural water withdrawal as % of total water withdrawal}{%}
+#' \item{Forest coverage}{%}
+#' \item{Forest land}{ha}
+#' \item{Forest plantation area}{ha}
+#' \item{Proportion of the population using improved drinking water sources, rural}
+#' \item{Proportion of the population using improved drinking water sources, urban}
+#' \item{Industrial water withdrawal as % of total water withdrawal}{%}
+#' \item{Municipal water withdrawal as % of total withdrawal}{%}
+#' \item{Municipal water withdrawal per capita}{m3/inhab/yr}
+#' \item{Primary forest land}{ha}
+#' \item{Total water withdrawal per capita}{m3/inhab/yr}
+#' \item{Total water withdrawal (summed by sector)}{10^9 m3/yr}
+#' \item{Water resources: total internal renewable per capita}{m3/inhab/yr}
+#' \item{Water resources: total renewable per capita (actual)}{m3/inhab/yr}
+#' \item{Wood products removal}{m3}
+#' \item{Surface area}{sq. km}
+#' }
 #'
-#' @format A data frame with 27 variables: \code{country}, \code{region},
-#'   \code{year}, \code{}
-#'
+#'@examples
+#'  agriculture_environment
 #'
 "agriculture_environment"
 
@@ -17,9 +43,65 @@ NULL
 #'
 #' Economics data of African countries from Gapminder.
 #'
+#' @source https://gapminder.org/data
+#' @format Data frame with columns
+#' \describe{
+#' \item{ODA received, total (constant 2010 US$)}{}
+#' \item{Exports of goods and services}{% of GDP}
+#' \item{External debt stocks}{% of GNI}
+#' \item{Foreign direct investment, net inflows}{% of GDP}
+#' \item{Foreign direct investment, net outflows}{% of GDP}
+#' \item{Income per person (fixed 2000 US$)}{}
+#' \item{Imports of goods and services}{% of GDP}
+#' \item{Market capitalization of listed companies}{% of GDP}
+#' \item{Debt servicing costs}{% of exports and net income from abroad}
+#' \item{Central bank discount rate}{annual %}
+#' \item{External debt stocks, total}{DOD, current US$}
+#' \item{GDP per capita}{}
+#' \item{GDP (constant 2000 US$)}{}
+#' \item{Economic growth over 10 years}{}
+#' \item{Total GDP, PPP}{}
+#' \item{Income per person (subsistance basket)}{}
+#' \item{Economic growth over the past 10 years}{}
+#' \item{Real GDP per Capita}{}
+#' \item{Income share held by lowest 20%}{}
+#' \item{GINI index}{}
+#' \item{Total expenditure on health as percentage of GDP (gross domestic product)}{}
+#' \item{Net barter terms of trade (2000 = 100)}{}
+#' \item{GDP per capity, 2005 ppp, WB data}{}
+#' \item{Aid received % of GNI}{}
+#' \item{Aid received per person (current US$)}{}
+#' \item{Economical infrastructure aid (% of total)}{}
+#' \item{GDP per employee, (constant 1990$)}{}
+#' \item{GDP per working hour, (constant 1990$)}{}
+#' \item{Government and civil society aid (% of total aid)}{}
+#' \item{General government expenditure on health as percentage of total government expenditure}{}
+#' \item{General government expenditure on health as percentage of total expenditure on health}{}
+#' \item{Human Development Index}{}
+#' \item{Health aid (% of total aid)}{}
+#' \item{Income share held by fourth 20%}{}
+#' \item{Income share held by highest 10%}{}
+#' \item{Income share held by highest 20%}{}
+#' \item{Income share held by lowest 10%}{}
+#' \item{Income share held by second 20%}{}
+#' \item{Income share held by third 20%}{}
+#' \item{GNI per capita, Atlas method (current US$)}{}
+#' \item{GNI per capita (constant 2000 US$)}{}
+#' \item{GNI per capita, PPP (current international $)}{}
+#' \item{Total GNI, PPP (current international $)}{}
+#' \item{GDP per capita growth}{annual %}
+#' \item{Industry, value added}{% of GDP}
+#' \item{Inflation, GDP deflator}{annual %}
+#' \item{Gross capital formation}{% of GDP}
+#' \item{Merchandise trade}{% of GDP}
+#' \item{Tax revenue}{% of GDP}
+#' \item{Total reserves}{% of total external debt}
+#' \item{Trade balance (current US$)}{}
+#' \item{Trade balance}{% of GDP}
+#'}
 #'
-#'
-#'
+#'@examples
+#'   economy
 #'
 "economy"
 
